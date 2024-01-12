f3da8b96ba3f030919cc83a6a8471973eceabe46
franco filippo <francofilip@hotmail.com>
2024-01-12 16:14:43 -0300

    anotaciones js

---
+++ js/index.js
@@ -0,0 +1,8 @@
+/*    let nombre = prompt("Ingrese su nombre");
+    let apellido = prompt("Ingrese su apellido");
+
+    let nombreCompleto = nombre +  " " + apellido;
+
+    console.log("Hola! como estas",  nombreCompleto);
+
+    alert("Hola! ¿como estas? "+ nombreCompleto); */
